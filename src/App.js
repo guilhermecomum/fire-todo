0d5f5895ec86ec756bae0b1f4f4e673a401e011d
Guilherme Guerra <guerrinha@comum.org>
2018-05-05 16:30:57 -0300

    Adds router and pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
-import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './page/Home'
+import SignUp from './page/SignUp'
+import SignIn from './page/SignIn'
+import ForgetPassword from './page/ForgetPassword'
+import Account from './page/Account'
 
 export default class App extends Component {
   render() {
     return (
-      <Container>
-        <div className="main-container">
-          <h1>Todo List</h1>
+      <Router>
+        <div>
+          <Route exact path="/" component={() => <Home />} />
+          <Route exact path="/sign-up" component={() => <SignUp />} />
+          <Route exact path="/sign-in" component={() => <SignIn />} />
+          <Route
+            exact
+            path="/forget-password"
+            component={() => <ForgetPassword />}
+          />
+          <Route exact path="/account" component={() => <Account />} />
         </div>
-      </Container>
+      </Router>
     )
   }
 }
